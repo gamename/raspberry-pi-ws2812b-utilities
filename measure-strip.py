2f41aa8210e94a444001a23a11678c1ce4b060a3
gamename <tennis.n.smith@gmail.com>
2022-09-15 08:18:35 -0500

    test for ints and for correct values in range

--- measure-strip.py
+++ measure-strip.py
@@ -8,7 +8,7 @@ There are probably better ways to do this, but it works for me.
 NOTE: You have to run this with sudo.
 
 Example:
-    sudo python3 ./measure-strip.py --max_pixels 110 --test_pixel 103
+    sudo python3 ./measure-strip.py --max-pixels 110 --test-pixel 103
 """
 import board
 import neopixel
@@ -21,24 +21,40 @@ OFF = (0, 0, 0)
 BRIGHTNESS = 1.0
 
 
+def restricted_int(x):
+    """
+    Restricts the value to be and integer
+    :param x: The value to be restricted
+    :return: The value if it is an integer
+    """
+    try:
+        x = int(x)
+    except ValueError:
+        raise argparse.ArgumentTypeError("%r not an integer" % (x,))
+    return x
+
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--max-pixels",
-                        type=int,
+                        type=restricted_int,
                         action="store",
                         required=True,
                         dest="max_pixels",
                         help="The maximum number of pixels in the strip")
 
     parser.add_argument("--test-pixel",
-                        type=int,
+                        type=restricted_int,
                         action="store",
                         required=True,
                         dest="test_pixel",
                         help="Turn on test pixel")
 
     args = parser.parse_args()
+
+    if args.test_pixel > args.max_pixels:
+        raise ValueError("Test pixel cannot be greater than the maximum number of pixels in the strip")
 
     pixels = neopixel.NeoPixel(board.D18, args.max_pixels, brightness=BRIGHTNESS)
 
